886ac3e25482fe54e372d10e6594d2c499dd42a1
sfjp23j2 <tutbxaoptpo@gmail.com>
2024-02-27 15:00:09 +0330

    create help menu & connect argument to function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import subprocess
 from colorama import Fore,init
 import zipfile
+import sys
 
 # intiziling colors
 init()
@@ -102,10 +103,11 @@ def banner():
                         {colors['YELLOW']}+------------------------------+
                         +     {author}       +
                         +------------------------------+
-
-                            --hide      hide file
-                            --unhide    extract file
-                            --show      show hide file
+{colors['WHITE']}----------------------------------------------------------------------------------------------
+--hide    (hide file) usage: <png_file> <ziped_file> <new_file_name>
+--unhide  (extract file) usage: <file_name>
+--show    (show hide file) usage: <file_name>
+                            
 
     """
     print(temp.encode('utf-8').decode('utf-8'),end='')
@@ -131,8 +133,29 @@ def show_hiden_file(file_name):
         zip.printdir()
 
 def main():
-    pass
+    arguments = sys.argv[1:] # show after python main.py
 
-# hide_file('pic.png','data.zip','meow.png')
-show_hiden_file('meow.png')
+    if len(arguments) == 0:
+        banner()
 
+    if '--hide' in arguments:
+        arg_index = arguments.index('--hide')
+        file_name = arguments[arg_index + 1]
+        ziped_file = arguments[arg_index + 2]
+        new_file_name = arguments[arg_index + 3]
+        hide_file(file_name,ziped_file,new_file_name)
+
+    if '--extract' in arguments:
+        arg_index = arguments.index('--extract')
+        file_name = arguments[arg_index + 1]
+        extract_file(file_name)
+        
+
+    if '--show' in arguments:
+        arg_index = arguments.index('--show')
+        file_name = arguments[arg_index + 1]
+        show_hiden_file(file_name)
+
+
+if __name__ == '__main__':
+    main()
